a1c3e908de1926f2555910a2c21901f247aeef09
hunzilah-anwar <hunzilahanwar786@gmail.com>
2025-06-10 19:07:13 +0500

    all ok

--- JS/style.js
+++ JS/style.js
@@ -21,3 +21,21 @@
         function closeMenu(){
             sidemenu.style.right = '-200px'
         }
+
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbyD53Red142gANRRvdgqwUFIWj6TpDBB2dwiK_LPzg5BKAj6971afwR7PnaCmHWFOV7Zw/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById('msg')
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = 'Message Sent Successfully !'
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
